c8501d15eec6e1a4c87826b82dff2620b038e202
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2014-09-28 14:45:02 -0600

    More docs for ConstantlySortedQueue

--- lib/wav_head_constantly_sorted_queue.rb
+++ lib/wav_head_constantly_sorted_queue.rb
@@ -1,8 +1,13 @@
 require 'thread'
 module WavHead
+  ##
+  # This class provides a queue that sorts itself on every operation.
+  # It is thread-safe.
   class ConstantlySortedQueue
     def initialize
+      # Internally, queue is stored as an array
       @array = []
+      # We have a mutex to protect this array for thread safety.
       @mut = Mutex.new
     end
     ##
@@ -15,20 +20,30 @@ module WavHead
     def insert(o)
       @mut.synchronize{@array << o}
     end
+    ##
+    # The item that will be returned by "pop" next.
     def next
       @mut.synchronize{@array.sort![-1]}
     end
-
+    ##
+    # Get the highestes element in the queue.
+    # This also removes the item from the queue.
     def pop
       @mut.synchronize{@array.sort!.pop}
     end
+    ##
+    # Give the top(num) items in the queue.
+    # So top(10) will give the top 10 items in the queue.
     def top(num)
       @mut.synchronize{@array.sort{|x,y| y <=> x}.take(num)}
-      
     end
+    ##
+    # See if an item is in our queue
     def include?(o)
       @mut.synchronize{@array.include?(o)}
     end
+    ##
+    # The size of the queue
     def size
       return @array.size
     end
