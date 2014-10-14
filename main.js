6b1d3f5f714858e160cc11af0902550d08ec5f8b
Connor Shea <connor.james.shea@gmail.com>
2014-10-14 00:04:44 -0600

    Scrolling works!

--- main.js
+++ main.js
@@ -3,13 +3,11 @@ window.onload = setup;
 function setup(){
   var windowheight = $(window).height();
 
-  console.log(windowheight);
-
-  var homecontainer = document.getElementById("homecontainer");
-  var designcontainer = document.getElementById("designcontainer");
-  var featurescontainer = document.getElementById("featurescontainer");
-  var installcontainer = document.getElementById("installcontainer");
-  var aboutcontainer = document.getElementById("aboutcontainer");
+  $("#homecontainer").css("height", windowheight - 60 + "px");
+  $("#designcontainer").css("height", windowheight - 60 + "px");
+  $("#featurescontainer").css("height", windowheight - 60 + "px");
+  $("#installcontainer").css("height", windowheight - 60 + "px");
+  $("#aboutcontainer").css("height", windowheight - 60 + "px");
 
   document.getElementById("wavheadtitle").addEventListener("click", homeScroll, false);
   document.getElementById("headeritems-design").addEventListener("click", designScroll, false);
@@ -20,21 +18,31 @@ function setup(){
 }
 
 function homeScroll(){
-
+  $('html, body').animate({
+    scrollTop: $("#homecontainer").offset().top
+  }, 1000);
 }
 
 function designScroll(){
-  
+  $('html, body').animate({
+    scrollTop: $("#designcontainer").offset().top
+  }, 1000);
 }
 
 function featuresScroll(){
-  
+  $('html, body').animate({
+    scrollTop: $("#featurescontainer").offset().top
+  }, 1000); 
 }
 
 function installScroll(){
-  
+  $('html, body').animate({
+    scrollTop: $("#installcontainer").offset().top
+  }, 1000);
 }
 
 function aboutScroll(){
-  
+  $('html, body').animate({
+    scrollTop: $("#aboutcontainer").offset().top
+  }, 1000);
 }
