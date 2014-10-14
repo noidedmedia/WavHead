b2e8082cf1f5fd032e12eaee689f6871e94771c0
Connor Shea <connor.james.shea@gmail.com>
2014-10-13 23:23:13 -0600

    Initial JavaScript, modified styles

---
+++ main.js
@@ -0,0 +1,36 @@
+window.onload = setup;
+
+function setup(){
+  var homecontainer = document.getElementById("homecontainer");
+  var designcontainer = document.getElementById("designcontainer");
+  var featurescontainer = document.getElementById("featurescontainer");
+  var installcontainer = document.getElementById("installcontainer");
+  var aboutcontainer = document.getElementById("aboutcontainer");
+
+  document.getElementById("wavheadtitle").addEventListener("click", homeScroll, false);
+  document.getElementById("headeritems-design").addEventListener("click", designScroll, false);
+  document.getElementById("headeritems-features").addEventListener("click", featuresScroll, false);
+  document.getElementById("headeritems-install").addEventListener("click", installScroll, false);
+  document.getElementById("headeritems-about").addEventListener("click", aboutScroll, false);
+
+}
+
+function homeScroll(){
+
+}
+
+function designScroll(){
+  
+}
+
+function featuresScroll(){
+  
+}
+
+function installScroll(){
+  
+}
+
+function aboutScroll(){
+  
+}
