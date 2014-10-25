57027bf67a86fb550ec84b0d392be5aaa5743f71
Connor Shea <connor.james.shea@gmail.com>
2014-10-24 20:32:02 -0600

    IT WORKS!
    Upon load, assuming you’re in the first section, it won’t show the
    header or the “WavHead” in the header. This is a stylistic choice, and
    I think it’s pretty nice looking.

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
-    $('#content').fullpage({
-      css3: true,
-      paddingTop: '60px',
-      easing: 'easeInCubic',
-      loopBottom: false,
-      loopTop: false,
-      loopHorizontal: false,
-      slidesNavigation: true,
-      slidesNavPosition: 'bottom',
-      menu: '#header',
-      anchors: ['home', 'design', 'features', 'install', 'about'],
-    });
+  $('#content').fullpage({
+    css3: true,
+    paddingTop: '60px',
+    easing: 'easeInCubic',
+    loopBottom: false,
+    loopTop: false,
+    loopHorizontal: false,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    menu: '#header',
+    anchors: ['home', 'design', 'features', 'install', 'about'],
+    onLeave: function(index, nextIndex, direction) {
+      if(nextIndex == 1) {
+        $("#headercontainer").css("background-color", "transparent");
+      }
+      else {
+        $("#headercontainer").css("background-color", "rgba(88,86,214,0.6)");
+      }
+    },
+  });
 });
